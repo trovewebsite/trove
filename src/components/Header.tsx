21cfadc8d663a4f5b9fa2cf6ea698dbd78ef3ae4
trove <trovewebsite@proton.me>
2025-10-09 15:03:52 -0400

    update Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,17 +6,16 @@ const Header: React.FC = () => {
   return (
     <div className="flex h-[70px] w-full max-w-[1200px] items-center justify-between rounded-[17px] border-x border-white/20 bg-white/15 py-3 pr-3 pl-5">
       <Image src="/icons/logo.svg" alt="logo" width={120} height={25} />
-
-      <div className="flex gap-4">
+      <div className="flex items-center gap-4">
         <Button
-          className="font-poppins text-primary-foreground text-base"
+          className="text-primary-foreground text-base"
           variant="ghost"
           onClick={() => {}}
         >
           Login
         </Button>
         <Button
-          className="font-poppins text-base text-black"
+          className="h-full text-base text-black"
           variant="default"
           onClick={() => {}}
         >
