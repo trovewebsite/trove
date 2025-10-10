95876300148f2645bd41cc487c1ee316ffdebdc4
trove <trovewebsite@proton.me>
2025-10-10 04:55:58 -0400

    update border style of Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 
 const Header: React.FC = () => {
   return (
-    <div className="flex h-[70px] w-full max-w-[1200px] items-center justify-between rounded-[17px] border-x border-white/20 bg-white/15 py-3 pr-3 pl-5">
+    <div className="flex h-[70px] w-full max-w-[1200px] items-center justify-between rounded-[17px] border border-white/20 bg-white/15 py-3 pr-3 pl-5">
       <Image src="/icons/logo.svg" alt="logo" width={120} height={25} />
       <div className="flex items-center gap-4">
         <Button
