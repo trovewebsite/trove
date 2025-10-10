3e9cce1edcdd178289b2a4c1392599378e5de085
trove <trovewebsite@proton.me>
2025-10-10 10:13:13 -0400

    add blur effect in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,23 +4,26 @@ import { Button } from "@/components/ui/button";
 
 const Header: React.FC = () => {
   return (
-    <div className="flex h-[70px] w-full max-w-[1200px] items-center justify-between rounded-[17px] border border-white/20 bg-white/15 py-3 pr-3 pl-5">
-      <Image src="/icons/logo.svg" alt="logo" width={120} height={25} />
-      <div className="flex items-center gap-4">
-        <Button
-          className="text-primary-foreground text-base"
-          variant="ghost"
-          onClick={() => {}}
-        >
-          Login
-        </Button>
-        <Button
-          className="h-full text-base text-black"
-          variant="default"
-          onClick={() => {}}
-        >
-          Launch App
-        </Button>
+    <div className="relative flex h-[70px] w-full max-w-[1200px] items-center py-3 pr-3 pl-5">
+      <div className="absolute inset-0 -z-10 rounded-[17px] border border-white/20 bg-white/15 backdrop-blur-xs" />
+      <div className="z-30 flex w-full items-center justify-between">
+        <Image src="/icons/logo.svg" alt="logo" width={120} height={25} />
+        <div className="flex items-center gap-4">
+          <Button
+            className="text-primary-foreground text-base"
+            variant="ghost"
+            onClick={() => {}}
+          >
+            Login
+          </Button>
+          <Button
+            className="h-full text-base text-black"
+            variant="default"
+            onClick={() => {}}
+          >
+            Launch App
+          </Button>
+        </div>
       </div>
     </div>
   );
