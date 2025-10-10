73ffea76ca0ef1c9966dc0e8c49d4dfdf92a3ca0
trove <trovewebsite@proton.me>
2025-10-10 10:16:21 -0400

    update position of Header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ import ReadySection from "@/components/ReadySection";
 export default function Home() {
   return (
     <div className="mx-auto min-h-screen w-full max-w-[1440px] px-2 lg:px-8 xl:px-16">
-      <div className="fixed top-8 right-0 left-0 z-10 flex w-full justify-center">
+      <div className="fixed top-16 right-0 left-0 z-10 flex w-full justify-center">
         <Header />
       </div>
       <div className="relative flex h-full w-full flex-col items-center">
